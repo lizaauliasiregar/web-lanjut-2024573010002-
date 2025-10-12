b8b26336ff1eb24fd862c9d39c74b2e38afcc3a0
Liza Aulia Siregar <lizaauliasiregar07@gmail.com>
2025-10-12 23:49:22 +0700

    Update Folder

---
+++ projects/modul-4-laravel-ui/resources/views/components/feature-card.blade.php
@@ -0,0 +1,20 @@
+@props([
+    'title' => 'Judul Default',
+    'description' => 'Description Default',
+    'icon' => '⭐',
+    'badge' => null,
+    'theme' => 'light'
+    
+])
+    <div class="card feature-card h-100 {{ $theme === 'dark' ? 'bg-secondary text-white' : '' }}">
+    <div class="card-body">
+        <div class="d-flex align-items-center mb-3">
+            <span class="fs-2 me-3">{{ $icon ?? '⭐' }}</span>
+            <h5 class="card-title mb-0">{{ $title }}</h5>
+        </div>
+        <p class="card-text">{{ $description }}</p>
+        @if(isset($badge))
+            <span class="badge {{ $theme === 'dark' ? 'bg-light text-dark' : 'bg-dark' }}">{{ $badge }}</span>
+        @endif
+    </div>
+</div>
